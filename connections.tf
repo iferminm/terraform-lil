263e7cfdda33739017c4eeef3ff5be0ef2354e44
Israel Fermin Montilla <ferminster@gmail.com>
2018-07-05 11:55:52 +0400

    Removes all google cloud stuff because it's not setup properly

--- connections.tf
+++ connections.tf
@@ -1,9 +1,3 @@
-provider "google" {
-    credentials = "${file("account.json")}"
-    project = "api-project-21979153790"
-    region = "us-west1"
-}
-
 provider "aws" {
     region = "eu-west-1"
 }
--- resources.tf
+++ resources.tf
@@ -1,8 +1,3 @@
-resource "google_compute_network" "out_development_network" {
-    name = "devnetwork"
-    auto_create_subnetworks = true
-}
-
 resource "aws_vpc" "environment-example-two" {
     cidr_block = "10.0.0.0/16"
     enable_dns_hostnames = true
